77961d668c6bd2877003e05e1a6629ae02cf4b1f
Martin Reinhardt <martin.reinhardt.87@gmail.com>
2022-05-09 16:27:25 +0200

    WIP changes

--- src/FilterGeneration.jl
+++ src/FilterGeneration.jl
@@ -1,25 +1,36 @@
 """
   GenerateRadius(filtersize)
 
-Generates the Radius for a given filtersize
+Generates the Radius for a given filtersize of any size
+"""
+function GenerateRadius
+  
+end #GenerateRadius
+
+"""
+  GenerateRadius2D(filtersize)
+
+Generates the Radius for a given 2D filtersize
 """
 function GenerateRadius2D(filtersize, type=Float64)
   # malloc
-  finalRadius = Array{type}(undef, filtersize)
-  radius_x = range(-0.5, 0.5, filtersize[1] + 1)
-  radius_x = radius_x[1:end-1]
-  radius_y = range(-0.5, 0.5, filtersize[2] + 1)
-  radius_y = radius_y[1:end-1]
-
+  radius_x = GenerateRadius1D(filtersize[1], type)
+  radius_y = GenerateRadius1D(filtersize[2], type)
   mesh_x = radius_x' .* ones(filtersize)
-  mesh_y = ones(filtersize) .* radius_y
-  radius = abs(mesh_x + im * mesh_y)
-  center_x = floor(filtersize(1) / 2 + 1)
-  center_y = floor(filtersize(2) / 2 + 1)
-
+  mesh_y = radius_y .* ones(filtersize)
+  radius = abs.(mesh_x + im * mesh_y)
+  center_x = floor(filtersize[1] / 2 + 1)
+  center_y = floor(filtersize[2] / 2 + 1)
   return (radius, mesh_x, mesh_y, center_x, center_y)
 end #GenerateRadius2D
 
-function GenerateRadius1D(filtersize, type=Float64)
+"""
+  GenerateRadius1D(filtersize)
 
+Generates the Radius for a given 1D filtersize
+"""
+function GenerateRadius1D(filtersize, type=Float64)
+  radius = range(-0.5, 0.5, filtersize + 1)
+  radius = radius[1:end-1]
+  return type.(radius)
 end #GenerateRadius1D
--- src/RieszFrames.jl
+++ src/RieszFrames.jl
@@ -3,7 +3,7 @@ module RieszFrames
 export held, papadakis, shannon, simoncelli
 include("FrameFunctions.jl")
 
-export GenerateRadius2D
+export GenerateRadius2D, GenerateRadius1D
 include("FilterGeneration.jl")
 
 end #module
