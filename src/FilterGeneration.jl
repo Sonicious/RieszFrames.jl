6a31e1998baeee2604849eef9a9b5dc995686ed9
Martin Reinhardt <martin.reinhardt.87@gmail.com>
2022-05-12 11:10:36 +0200

    Debugging in Filtergeneration.

--- src/FilterGeneration.jl
+++ src/FilterGeneration.jl
@@ -34,7 +34,7 @@ function GenerateFrameFilters2D(filtersize, maxscale=8, framefunction=Held, type
         (radius .* 2^decompositionscheme[1] .<= 0.25) +
         framefunction.(2^decompositionscheme[1] .* radius) .* (radius .* 2^decompositionscheme[1] .> 0.25) # lowpass
     filterbank[length(decompositionscheme)] =
-        1 * (radius .* 2^decompositionscheme[end] .>= 0.25) + 
+        1 * (radius .* 2^decompositionscheme[end] .>= 0.25) +
         framefunction.(2^decompositionscheme[end] .* radius) .* (radius .* 2^decompositionscheme[end] .< 0.25) # highpass
     return filterbank
 end
@@ -49,8 +49,11 @@ function GenerateRieszFilters2D(filtersize, maxrieszorder=1, type=Float64)
     if maxrieszorder == 1
         filterbank = Array{Array{Complex{type}}}(undef, 2)
         radius, mesh_x, mesh_y, center_x, center_y = GenerateRadius2D(filtersize, type)
-        filterbank[1] = mesh_x .* 1im ./ radius
-        filterbank[1] = mesh_y .* 1im ./ radius
+        filterbank[1] = mesh_x .* -1im ./ radius
+        filterbank[1][center_x, center_y] = zero(type)
+        filterbank[2] = mesh_y .* -1im ./ radius
+        filterbank[2][center_x, center_y] = zero(type)
+        return filterbank
     end
     # calculate the size of the filter pyramid
     # it is guaranteed that this will be an Int
@@ -61,7 +64,7 @@ function GenerateRieszFilters2D(filtersize, maxrieszorder=1, type=Float64)
     # create filterbank filter by filter and save it into the filterbank
     # the indexing method seems not consistend, but it fits together
     # Another approach would be to use  complex indexing inside a Pascal triangle, which is slower (benchmarked already)
-    rieszfilteridx = 0
+    rieszfilter_idx = 0
     for rieszorder = 1:maxrieszorder
         #complexify radius and prepare for division to have just one division per loop. Maybe more optimization is possible here
         radiusfactor = ((1 ./ radius) .* (-1im)) .^ rieszorder
@@ -70,7 +73,7 @@ function GenerateRieszFilters2D(filtersize, maxrieszorder=1, type=Float64)
         riesz_idx_y = rieszorder .- riesz_idx_x
         # level starts
         for generation_idx = 1:rieszorder+1
-            rieszfilter_idx = rieszfilteridx + 1
+            rieszfilter_idx = rieszfilter_idx + 1
             filterbank[rieszfilter_idx] = ((mesh_x .^ riesz_idx_x[generation_idx]) .* (mesh_y .^ riesz_idx_y[generation_idx])) .* radiusfactor
             # deal with the center pixel which must be 0 by limit process.
             filterbank[rieszfilter_idx][center_x, center_y] = zero(type)
@@ -88,7 +91,7 @@ Generates the Radius for a given 2D filtersize
 function GenerateRadius2D(filtersize, type=Float64)
     # malloc
     radius_x = GenerateRadius1D(filtersize[1], type)
-    radius_y = GenerateRadius1D(filtersize[2], type)
+    radius_y = -GenerateRadius1D(filtersize[2], type) # because of orientation on the axis and on images
     mesh_x = radius_x' .* ones(filtersize)
     mesh_y = radius_y .* ones(filtersize)
     radius = abs.(mesh_x + im * mesh_y)
